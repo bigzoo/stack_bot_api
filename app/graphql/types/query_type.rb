b376c4449f0fa353f393e5b0efbf8501599ed513
Chris Nyaga <nyagachris75@gmail.com>
2017-11-21 03:33:49 +0300

    Created a graphql root query that handles users search requests .

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,12 @@ Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
-
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
+  # A query type for searches
+  field :search, types[ItemType] do
+    argument :query , !types.String
+    resolve -> (obj, args, ctx) {
+      StackExchange.search(args[:query])
     }
   end
+
 end
